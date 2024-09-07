bab0384d593b43ca1e0b9babd7d2704e9a6cdbe9
Nikom <pouriaom@gmail.com>
2024-09-07 14:23:33 +0330

    Add Power math solver

---
+++ Power math solver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Power_math_solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\t *** POWER MATH SOLVER ***");
+            Console.ResetColor();
+            Console.WriteLine("Please first Enter your base number then exponent number::");
+            Console.Write("   --->> ");
+            var baseNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("   --->> ");
+            var pNumber = Convert.ToDouble(Console.ReadLine());
+
+            var powerResult = POW(baseNumber, pNumber);
+            Console.WriteLine("Your result is equal=  " + powerResult);
+            System.Console.WriteLine();
+        }
+        static double POW(double x, double y)
+        {
+            double temp = 1;
+            for (int i = 0; i < y; i++)
+            {
+                temp *= x;
+            }
+            return temp;
+        }
+    }
+}
